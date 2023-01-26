2854c973136fef8f836d59e3b861758d696b7640
alexandr.myagkov <asmyagkov@lanit.ru>
2023-01-26 12:00:44 +0300

    init commit with ready first homework

---
+++ src/main/java/com/otus/spaceBattle/command/Rotate.java
@@ -0,0 +1,22 @@
+package com.otus.spaceBattle.command;
+
+import com.otus.spaceBattle.action.Rotatable;
+import java.util.Objects;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Rotate {
+    private final @NonNull Rotatable rotatable;
+
+    public void execute() {
+        rotatableDirectionNullCheck();
+        rotatable.setDirection(rotatable.getDirection().next(rotatable.getAngularVelocity()));
+    }
+
+    private void rotatableDirectionNullCheck() {
+        if (Objects.isNull(rotatable.getDirection())) {
+            throw new RuntimeException("Direction in rotatable can't be null");
+        }
+    }
+}
